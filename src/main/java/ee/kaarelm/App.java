8166c440d2c52f8545d2b2cbe7f3b8592d7e458a
Kaarel Mäearu <kaarel.maearu@icefire.ee>
2015-12-18 17:40:59 +0200

    Esimene commit

---
+++ src/main/java/ee/kaarelm/App.java
@@ -0,0 +1,18 @@
+package ee.kaarelm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ApplicationContext ctx =  new ClassPathXmlApplicationContext("META-INF/spring/publisherWsClient-config.xml");
+
+    }
+}
